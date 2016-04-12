35705e4977dfe3e31cce1d8a5c493dbd76374990
cassac <hicassac@gmail.com>
2016-04-11 17:30:44 -0700

    adds Candidate to admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Candidate
 
-# Register your models here.
+class CandidateAdmin(admin.ModelAdmin):
+	pass
+admin.site.register(Candidate, CandidateAdmin)
