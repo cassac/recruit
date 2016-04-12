08ddd4c09dd767d3156046bbd70fea6eff15a099
cassac <hicassac@gmail.com>
2016-04-11 17:46:41 -0700

    adds timezone choices to baseuser

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,22 @@
+import pytz
+
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
 from django_countries.fields import CountryField
+from django.utils import timezone
+
+TIMEZONE_CHOICES = tuple((choice, choice) for choice in pytz.common_timezones)
 
 class BaseUser(AbstractBaseUser):
 	email = models.EmailField(max_length=100, unique=True)
 	first_name = models.CharField(max_length=50, blank=False)
 	last_name = models.CharField(max_length=50, blank=False)
 	citizenship = CountryField(blank=False)
-	timezone = models.CharField(max_length=50, blank=False)
+	timezone = models.CharField(
+			max_length=50, 
+			blank=False,
+			choices=TIMEZONE_CHOICES,
+		)
 	is_active = models.BooleanField(default=True)
 	is_admin = models.BooleanField(default=False)
 
