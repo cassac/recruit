a03b030af9ebdbc5fc9ff42d371d13b0cc7ed3a8
cassac <hicassac@gmail.com>
2016-04-11 22:11:57 -0700

    adds candidate preferences to admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Candidate
+from .models import Candidate, CandidatePreferences, CandidatePreferencesLocation
 
 class CandidateAdmin(admin.ModelAdmin):
 	exclude = ('password', 'last_login', 'is_admin',)
 
 admin.site.register(Candidate, CandidateAdmin)
+
+class CandidatePreferencesAdmin(admin.ModelAdmin):
+	pass
+
+admin.site.register(CandidatePreferences, CandidatePreferencesAdmin)
