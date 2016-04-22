12da4b64ba94bdba1664643e83b2bf87abdd0243
cassac <hicassac@gmail.com>
2016-04-22 09:38:20 -0700

    adds recruiter and company preferences to admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Candidate, CandidatePreferences, Recruiter
+from .models import (Candidate, CandidatePreferences, Recruiter, 
+	Company, CompanyPreferences)
 
 class CandidateAdmin(admin.ModelAdmin):
 	exclude = ('password', 'last_login', 'is_admin',)
@@ -15,3 +16,13 @@ class RecruiterAdmin(admin.ModelAdmin):
 	exclude = ('password', 'last_login', 'is_admin',)
 
 admin.site.register(Recruiter, RecruiterAdmin)
+
+class CompanyAdmin(admin.ModelAdmin):
+	exclude = ('password', 'last_login', 'is_admin',)
+
+admin.site.register(Company, CompanyAdmin)
+
+class CompanyPreferencesAdmin(admin.ModelAdmin):
+	pass
+
+admin.site.register(CompanyPreferences, CompanyPreferencesAdmin)
