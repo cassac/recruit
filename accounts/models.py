3ef219a94db00db285aeb1659a929226a071df67
cassac <hicassac@gmail.com>
2016-04-22 09:02:03 -0700

    adds Recruiter model class, makes Countries available to other m2m relationships

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.utils import timezone
 from django_countries.fields import CountryField
+from phonenumber_field.modelfields import PhoneNumberField
+
 from .choices import (TIMEZONE_CHOICES, COUNTRY_CHOICES, GENDER_CHOICES,
 	EDUCATION_CHOICES, EMPLOYER_TYPE_CHOICES, POSITION_TYPE_CHOICES, 
 	DESIRED_MONTHLY_SALARY_CHOICES)
@@ -38,7 +40,7 @@ class Candidate(BaseUser):
 	def __str__(self):
 		return self.email
 
-class CandidatePreferencesLocation(models.Model):
+class Countries(models.Model):
 	country = models.CharField(
 		max_length=100,
 		choices=COUNTRY_CHOICES,
@@ -68,7 +70,7 @@ class CandidatePreferences(models.Model):
 		Candidate,
 		on_delete=models.CASCADE,
 	)
-	location = models.ManyToManyField(CandidatePreferencesLocation, blank=True)
+	location = models.ManyToManyField(Countries, blank=True)
 	employer_type = models.ManyToManyField(CandidatePreferencesEmployerType, blank=True)
 	desired_job_title = models.ManyToManyField(CandidatePreferencesPositionType, blank=True)
 	desired_monthlty_salary = models.CharField(
@@ -79,3 +81,17 @@ class CandidatePreferences(models.Model):
 
 	def __str__(self):
 		return self.candidate.email
+
+class Recruiter(BaseUser):
+	phone_number = PhoneNumberField()
+	date_of_birth = models.DateField(blank=True)
+	location = models.CharField(blank=True, max_length=100)
+	id_card = models.ImageField(upload_to='%Y/%m/%d')
+	# candidate = models.OneToOneField(
+	# 	Employer,
+	# 	on_delete=models.CASCADE,
+	# )
+	USERNAME_FIELD = 'pk'
+
+	def __str__(self):
+		return self.email
