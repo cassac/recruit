6126d27e1d427f77ff2ab4de415122d3c34bba40
cassac <hicassac@gmail.com>
2016-04-22 10:02:49 -0700

    configures static and media directories

--- recruit/settings.py
+++ recruit/settings.py
@@ -122,5 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "media")
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "static_root")
+
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(BASE_DIR), "static", "static_files"),
+)
 
 PHONENUMBER_DB_FORMAT = 'E164'
