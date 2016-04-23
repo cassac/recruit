5c7144130e5f3209c374326ad4a3adb30244f54e
cassac <hicassac@gmail.com>
2016-04-23 12:11:39 -0700

    adds availability and exclusion class models. adds parameter to interviewrequest mclass model

--- interviews/models.py
+++ interviews/models.py
@@ -21,9 +21,22 @@ class InterviewRequest(models.Model):
 	party_b = models.OneToOneField(BaseUser, related_name='party_b')
 	confirmed_time = models.DateTimeField(null=True, blank=True)
 	status = models.IntegerField(choices=STATUS_CHOICES, default=0)
+	request_reminders_sent = models.IntegerField(default=0)
 	is_active = models.BooleanField(default=1)
 	last_modified = models.DateTimeField(auto_now_add=False, auto_now=True)
 	created = models.DateTimeField(auto_now_add=True, auto_now=False)
 
 	def __str__(self):
-		return '<Interview: A: %s, B: %s' % (self.party_a, self.party_b)
+		return '<Interview: A: %s, B: %s>' % (self.party_a, self.party_b)
+
+class Availability(models.Model):
+	day_of_week = models.IntegerField()
+	timeslot_begin = models.TimeField()
+	timeslot_end = models.TimeField()
+	baseuser = models.OneToOneField(BaseUser)
+	last_modified = models.DateTimeField(auto_now_add=False, auto_now=True)
+	created = models.DateTimeField(auto_now_add=True, auto_now=False)	
+
+class Exclusion(models.Model):
+	date = models.DateField()
+	baseuser = models.OneToOneField(BaseUser)
