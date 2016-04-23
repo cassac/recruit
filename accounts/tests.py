6963a764a056d08a4813368272447fce14ad5a55
cassac <hicassac@gmail.com>
2016-04-22 22:20:04 -0700

    adds test for base user

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,7 +1,30 @@
 import datetime
 
 from django.test import TestCase
-from .models import Candidate
+from .models import BaseUser, Candidate
+
+def get_base_user():
+	return BaseUser.objects.get(first_name='John')
+
+class BaseUserTestCase(TestCase):
+	def setUp(self):
+		baseuser = BaseUser.objects.create(
+				email='email@example.com',
+				first_name='John',
+				last_name='Doe',
+			)
+		baseuser.set_password('password')
+		baseuser.is_admin=True
+		baseuser.save()
+
+	def test_baseuser_creation(self):
+		baseuser = get_base_user()
+		self.assertEqual(baseuser.email, 'email@example.com')
+		self.assertEqual(baseuser.first_name, 'John')
+
+	def test_baseuser_password(self):
+		baseuser = get_base_user()
+		self.assertTrue(baseuser.check_password('password'))
 
 class CandidateTestCase(TestCase):
 	def setUp(self):
@@ -11,7 +34,8 @@ class CandidateTestCase(TestCase):
 				last_name='Doe',
 				citizenship='Antarctica',
 				timezone='Antarctica',
-				date_of_birth=datetime.date(1970, 1, 1)
+				date_of_birth=datetime.date(1970, 1, 1),
+				available=datetime.datetime.today()
 			)
 
 	def test_candidate_creation(self):
