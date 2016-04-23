405340f5aabb833b0dcfa428ae34b02a6f971b77
cassac <hicassac@gmail.com>
2016-04-22 22:00:11 -0700

    adds job preferences to jobs admin panel

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Job
+from .models import Job, JobPreferences
+
+class JobPreferencesInline(admin.StackedInline):
+	model = JobPreferences
+	can_delete = False
+	verbose_name_plural = 'Preferences'
 
 class JobAdmin(admin.ModelAdmin):
-	pass
+	inlines = (JobPreferencesInline,)
 
 admin.site.register(Job, JobAdmin)
