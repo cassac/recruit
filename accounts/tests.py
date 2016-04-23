f7b065d86a53fd3c091a8ccd8ba4c3b81cb1119a
cassac <hicassac@gmail.com>
2016-04-23 13:06:15 -0700

    adds login test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,10 +1,13 @@
 import datetime
 
-from django.test import TestCase
+from django.test import TestCase, Client
 from .models import BaseUser, Candidate
 
-def get_base_user():
-	return BaseUser.objects.get(first_name='John')
+def get_baseuser(id):
+	return BaseUser.objects.get(id=id)
+
+def get_candidate(id):
+	return Candidate.objects.get(id=id)
 
 class BaseUserTestCase(TestCase):
 	def setUp(self):
@@ -18,17 +21,19 @@ class BaseUserTestCase(TestCase):
 		baseuser.save()
 
 	def test_baseuser_creation(self):
-		baseuser = get_base_user()
+		baseuser = get_baseuser(1)
 		self.assertEqual(baseuser.email, 'email@example.com')
 		self.assertEqual(baseuser.first_name, 'John')
 
 	def test_baseuser_password(self):
-		baseuser = get_base_user()
+		baseuser = get_baseuser(1)
 		self.assertTrue(baseuser.check_password('password'))
 
 class CandidateTestCase(TestCase):
 	def setUp(self):
-		Candidate.objects.create(
+		self.client = Client()
+
+		candidate = Candidate.objects.create(
 				email='email@example.com',
 				first_name='John',
 				last_name='Doe',
@@ -37,8 +42,16 @@ class CandidateTestCase(TestCase):
 				date_of_birth=datetime.date(1970, 1, 1),
 				available=datetime.datetime.today()
 			)
+		candidate.set_password('password')
+		candidate.save()
 
 	def test_candidate_creation(self):
 		candidate = Candidate.objects.get(first_name='John')
 		self.assertEqual(candidate.email, 'email@example.com')
 		self.assertEqual(candidate.first_name, 'John')
+
+	def test_candidate_unauthorized_login(self):
+		candidate = get_candidate(1)
+		response = self.client.post('/admin/login/', {'email': candidate.email, 
+												'password': 'password'})
+		self.assertTrue(response.status_code==200)
