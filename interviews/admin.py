b4fd0d189c7c47099ba4d77af74d87a7bfe0712b
cassac <hicassac@gmail.com>
2016-04-23 13:06:00 -0700

    adjusts admin panel

--- interviews/admin.py
+++ interviews/admin.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 from .models import InterviewRequest
 
 class InterviewRequestAdmin(admin.ModelAdmin):
-	search_fields = ('party_a', 'party_b')
-	list_display = ('party_a', 'party_b', 'status', 'confirmed_time')	
+	readonly_fields = ('uuid',)
+	search_fields = ('uuid', 'party_a', 'party_b')
+	list_display = ('party_a', 'party_b', 'status', 'confirmed_time', 'uuid')	
 	list_filter = ('status', 'confirmed_time',)
 
 admin.site.register(InterviewRequest, InterviewRequestAdmin)
