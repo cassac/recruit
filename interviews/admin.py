5216d20cc7905129fde7a640f2cbb97edfbc05e7
cassac <hicassac@gmail.com>
2016-04-23 11:47:41 -0700

    adds functionality to interview admin panel

--- interviews/admin.py
+++ interviews/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from .models import InterviewRequest
 
 class InterviewRequestAdmin(admin.ModelAdmin):
-	pass
+	search_fields = ('party_a', 'party_b')
+	list_display = ('party_a', 'party_b', 'status', 'confirmed_time')	
+	list_filter = ('status', 'confirmed_time',)
 
 admin.site.register(InterviewRequest, InterviewRequestAdmin)
