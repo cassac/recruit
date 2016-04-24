f1af9621df37930b48311cd4e634fe85a8c17ef5
cassac <hicassac@gmail.com>
2016-04-23 23:30:40 -0700

    adjusts view for availability

--- interviews/views.py
+++ interviews/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.contrib import messages
 
-def available(request):
+from accounts.models import BaseUser
+
+def available(request, bu_id):
+	user = BaseUser.objects.get(id=bu_id)
 	messages.add_message(request, messages.SUCCESS, 'Message here')
-	context = {'foo': 'bar'}
+	context = {'user': user.first_name}
 	return render(request, 'interviews/available.html', context)
