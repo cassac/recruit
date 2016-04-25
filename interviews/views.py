1f48b1030864367a326db6beb42ed6e5960b85dc
cassac <hicassac@gmail.com>
2016-04-25 12:25:36 -0700

    handles get and post requests

--- interviews/views.py
+++ interviews/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.contrib import messages
+from django.http import JsonResponse
 
 from accounts.models import BaseUser
 
 def available(request, bu_id):
 	user = BaseUser.objects.get(id=bu_id)
-	messages.add_message(request, messages.SUCCESS, 'Message here')
-	context = {'user': user.first_name, 'timezone': 'America/Adak'}
+
+	if request.method == 'GET':
+		context = {'user': user.first_name, 'timezone': ''}
+
+	if request.method == 'POST':
+		context = {'message': 'success'}
+		return JsonResponse({'message':'Availability Updated'})
+
 	return render(request, 'interviews/available.html', context)
