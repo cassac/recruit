60f3afe36c8e23043bf45a8e2b6c968e94dc83a6
cassac <hicassac@gmail.com>
2016-04-26 08:50:24 -0700

    completes get request and returns json

--- interviews/views.py
+++ interviews/views.py
@@ -6,11 +6,26 @@ from django.http import JsonResponse
 from interviews.models import Available
 from accounts.models import BaseUser
 
+# {'3': [{'end': '05:30', 'start': '00:00'}, {'end': '21:30', 'start': '15:00'}], 
+# '1': [{'end': '18:00', 'start': '17:00'}], '0': [{'end': '21:00', 'start': '15:00'}],
+#  '2': [{'end': '23:59', 'start': '23:00'}]}
+
+# ad:  {'2': [{'start': '23:00', 'end': '23:59'}], '3': [{'start': '00:00', 'end': '05:30'}, {'start': '15:00', 'end': '21:30'}], '1': [{'start': '17:00', 'end': '18:00'}], '0': [{'start': '15:00', 'end': '21:00'}]}
+
 def available(request, bu_id):
 	user = BaseUser.objects.get(id=bu_id)
-
 	if request.method == 'GET':
-		context = {'user': user.first_name, 'timezone': ''}
+		user_availabiity = user.available_set.all()
+		availability_dict = {}
+		for avail in user_availabiity:
+			temp = {"start": avail.time_start, "end": avail.time_end}
+			day_of_week = str(avail.day_of_week)
+			if day_of_week not in availability_dict:
+				availability_dict[day_of_week] = []
+			availability_dict[day_of_week].append(temp)
+		availability = json.dumps(availability_dict)
+		print(availability)
+		context = {'user': user, 'availability': availability}
 
 	if request.method == 'POST':
 		old_availability = user.available_set.all()
