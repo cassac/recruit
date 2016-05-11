875e7d6481212f7a52cfca8cd23e19028ef7b987
cassac <hicassac@gmail.com>
2016-05-11 15:50:30 -0700

    adds unique constraint validation on user email

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,35 +4,24 @@ from django.contrib.auth.models import User
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
 
 from .models import (Candidate, CandidateRequirements, Recruiter, Employer, 
 	EmployerRequirements)
 
 class UserCreationForm(forms.ModelForm):
-	password1 = forms.CharField(label='Password',
-		widget=forms.PasswordInput)
-	password2 = forms.CharField(label='Password confirmation',
-		widget=forms.PasswordInput)
+
 
 	class Meta:
 		model = User
-		fields = ('email',)
+		fields = ('email', 'username')
 
-	def clean_password2(self):
-		password1 = self.cleaned_data.get('password1')
-		password2 = self.cleaned_data.get('password2')
-
-		if password1 and password2 and password1 != password2:
-			raise forms.ValidationError('Passwords don\'t match')
-
-		return password2
-
-	def save(self, commit=True):
-		user = super(UserCreationForm, self).save(commit=False)
-		user.set_password(self.cleaned_data['password1'])
-		if commit:
-			user.save()
-		return user
+	def clean(self):
+		super(UserCreationForm, self).clean()
+		cleaned_data = self.cleaned_data
+		if User.objects.filter(email=cleaned_data['email']).exists():
+			raise ValidationError('Email already registered')
+		return cleaned_data
 
 class UserChangeForm(forms.ModelForm):
 	password = ReadOnlyPasswordHashField()
@@ -57,7 +46,7 @@ class UserAdmin(BaseUserAdmin):
 	add_fieldsets = (
 		(None, {
 		'classes': ('wide',),
-		'fields': ('email', 'password1', 'password2')}
+		'fields': ('email','username')}
 		),
 	)
 	search_fields = ('email',)
