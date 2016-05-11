0485675cf081dd902ac7e6dc9dec21d431a7a031
cassac <hicassac@gmail.com>
2016-05-10 23:48:41 -0700

    removes custom baseuser for django auth User model

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import User
 from django.utils import timezone
+
 from django_countries.fields import CountryField
 from phonenumber_field.modelfields import PhoneNumberField
 
@@ -8,91 +9,25 @@ from .choices import (TIMEZONE_CHOICES, COUNTRY_CHOICES, GENDER_CHOICES,
 	EDUCATION_CHOICES, EMPLOYER_TYPE_CHOICES, POSITION_TYPE_CHOICES, 
 	DESIRED_MONTHLY_SALARY_CHOICES)
 
-def load_m2m_choices():
 
-	for choice in COUNTRY_CHOICES:
-		Country(
-			country=choice[1]
-		).save()
-
-	for choice in GENDER_CHOICES:
-		Gender(
-			gender=choice[1]
-		).save()
-
-	for choice in EMPLOYER_TYPE_CHOICES:
-
-		CandidatePreferencesEmployerType(
-				employer_type=choice[1]
-			).save()
-
-	for choice in POSITION_TYPE_CHOICES:
-
-		CandidatePreferencesPositionType(
-				job_title=choice[1]
-			).save()
-
-class BaseUserManager(BaseUserManager):
-	def create_user(self, email, password=None):
-		if not email:
-			raise ValueError('Users must have an email address')
-
-		user = self.model(
-			email=self.normalize_email(email),
-		)
-		user.set_password(password)
-		user.save(using=self._db)
-		return user
-
-	def create_superuser(self, email, password):
-		user = self.create_user(email,
-			password=password
-		)
-		user.is_admin = True
-		user.save(using=self._db)
-		return user
-
-class BaseUser(AbstractBaseUser):
-	email = models.EmailField(max_length=100, unique=True, verbose_name='email address')
-	first_name = models.CharField(max_length=50, blank=False)
-	last_name = models.CharField(max_length=50, blank=False)
-	gender = models.CharField(max_length=10, blank=True, choices=GENDER_CHOICES)
-	citizenship = CountryField(blank=False)
-	timezone = models.CharField(
-			max_length=50, 
-			blank=False,
-			choices=TIMEZONE_CHOICES,
-		)
-	is_active = models.BooleanField(default=True)
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	timezone = models.CharField(max_length=50, blank=True)
+	citizenship = models.CharField(max_length=50, blank=True)
+	user_type = models.CharField(
+		choices = (('Candidate', 'Candidate'), ('Recruiter', 'Recruiter'), ('Employer', 'Employer'),),
+		max_length = 50,
+		default = False,
+	)
 	is_admin = models.BooleanField(default=False)
 	last_modified = models.DateTimeField(auto_now_add=False, auto_now=True)
 	created = models.DateTimeField(auto_now_add=True, auto_now=False)
 
-	objects = BaseUserManager()
-
-	USERNAME_FIELD = 'email'
-
-	def __str__(self):
-		return self.email
-
-	def get_full_name(self):
-		return self.email
-
-	def get_short_name(self):
-		return self.email
-
-	def has_perm(self, perm, obj=None):
-		return True
-
-	def has_module_perms(self, app_label):
-		return True
-
-	@property
-	def is_staff(self):
-		return self.is_admin
-
-class Candidate(BaseUser):
+class Candidate(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	birth_year = models.CharField(max_length=4, blank=False)
 	date_of_birth = models.DateField(blank=True, null=True)
+	gender = models.CharField(choices=(('male', 'Male'), ('female', 'Female'),), max_length = 10, blank=True, null=True)
 	education = models.CharField(
 			max_length=25,
 			blank=True,
@@ -100,79 +35,28 @@ class Candidate(BaseUser):
 		)	
 	education_major = models.CharField(max_length=250, blank=True)
 	current_location = CountryField(blank=True)
-	available_after = models.DateField()
-	USERNAME_FIELD = 'pk'
 
-	def __str__(self):
-		return self.email
-
-class Country(models.Model):
-	country = models.CharField(
-		max_length=100,
-		choices=COUNTRY_CHOICES,
-	)
-
-	def __str__(self):
-		return self.country
-
-class Gender(models.Model):
-	gender = models.CharField(
-		max_length=100,
-		choices=GENDER_CHOICES,
-	)
-
-	def __str__(self):
-		return self.gender
-
-class CandidatePreferencesEmployerType(models.Model):
+class CandidateRequirements(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	# location = models.ManyToManyField(Location, on_delete=models.CASCADE)  create m2m
 	employer_type = models.CharField(
-		max_length=100,
-		choices=EMPLOYER_TYPE_CHOICES,
-	)
-	def __str__(self):
-		return self.employer_type
+			max_length=25,
+			blank=True,
+			choices=EMPLOYER_TYPE_CHOICES,
+		)
 
-class CandidatePreferencesPositionType(models.Model):
-	job_title = models.CharField(
-		max_length=10,
-		choices=POSITION_TYPE_CHOICES
-	)
-	def __str__(self):
-		return self.job_title				
 
-class CandidatePreferences(models.Model):
-	candidate = models.OneToOneField(
-		Candidate,
-		on_delete=models.CASCADE,
-	)
-	location = models.ManyToManyField(Country, blank=True)
-	employer_type = models.ManyToManyField(CandidatePreferencesEmployerType, blank=True)
-	desired_job_title = models.ManyToManyField(CandidatePreferencesPositionType, blank=True)
-	desired_monthlty_salary = models.CharField(
-		blank=True,
-		max_length=10,
-		choices=DESIRED_MONTHLY_SALARY_CHOICES
-	)
-
-	def __str__(self):
-		return self.candidate.email
-
-class Company(BaseUser):
+class Employer(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)	
 	phone_number = PhoneNumberField(blank=False)
 	name_english = models.CharField(blank=False, max_length=200)
 	name_local = models.CharField(blank=False, max_length=200)
 	address_english = models.CharField(blank=False, max_length=200)
 	address_local = models.CharField(blank=False, max_length=200)
-	business_license = models.ImageField(upload_to='%Y/%m/%d')
+	business_license = models.ImageField(upload_to='employer/%Y/%m/%d')
 
-	def __str__(self):
-		return self.name_english
-
-class CompanyPreferences(models.Model):
-	company = models.OneToOneField(
-		Company,
-		on_delete=models.CASCADE,
-	)
+class EmployerRequirements(models.Model):
+	employer = models.OneToOneField(Employer, on_delete=models.CASCADE)
 	education = models.CharField(
 			max_length=25,
 			blank=True,
@@ -182,18 +66,14 @@ class CompanyPreferences(models.Model):
 	age_range_low = models.IntegerField(blank=True)
 	age_range_high = models.IntegerField(blank=True)
 	years_of_experience = models.IntegerField(blank=True)
-	citizenship = models.ManyToManyField(Country, blank=True)
+	citizenship = CountryField(blank=True)
 
-	def __str__(self):
-		return self.company.name_english
-
-class Recruiter(BaseUser):
+class Recruiter(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	phone_number = PhoneNumberField(blank=False)
 	date_of_birth = models.DateField(blank=True, null=True)
 	location = models.CharField(blank=True, max_length=100)
-	id_card = models.ImageField(upload_to='%Y/%m/%d')
-	companies = models.ManyToManyField(Company, blank=False)
-	USERNAME_FIELD = 'pk'
+	id_card = models.ImageField(upload_to='recruiter/%Y/%m/%d')
 
 	def __str__(self):
-		return self.email
+		return self.user.email
