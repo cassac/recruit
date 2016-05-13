427b07f5a86cfe4c6303f3e9e51e64ff26ce3657
cassac <hicassac@gmail.com>
2016-05-12 21:22:20 -0700

    experimenting with custom save for thumbnamil

--- accounts/models.py
+++ accounts/models.py
@@ -1,14 +1,14 @@
+from PIL import Image
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-
 from django_countries.fields import CountryField
 from phonenumber_field.modelfields import PhoneNumberField
 
 from .choices import (TIMEZONE_CHOICES, COUNTRY_CHOICES, GENDER_CHOICES,
 	EDUCATION_CHOICES, EMPLOYER_TYPE_CHOICES, POSITION_TYPE_CHOICES, 
 	DESIRED_MONTHLY_SALARY_CHOICES)
-
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -60,9 +60,23 @@ class Employer(models.Model):
 	address_english = models.CharField(blank=False, max_length=200)
 	address_local = models.CharField(blank=False, max_length=200)
 	business_license = models.ImageField(upload_to='employer/%Y/%m/%d')
+	business_license_thumb = models.ImageField(upload_to='employer/%Y/%m/%d', blank=True)
 
 	def __str__(self):
 		return self.name_english
+
+	def save(self, *args, **kwargs):
+		from .utils import generate_thumbnail
+		thumb,filename=generate_thumbnail(self.business_license)
+		# print(thumb.filename, thumb.size)
+		# print(dir(thumb))
+		# print(thumb)
+		# print(dir(self.business_license_thumb))
+		self.business_license_thumb=thumb
+
+		
+
+		super(Employer, self).save(*args, **kwargs)
 
 class EmployerRequirements(models.Model):
 	employer = models.OneToOneField(Employer, on_delete=models.CASCADE)
@@ -86,3 +100,30 @@ class Recruiter(models.Model):
 
 	def __str__(self):
 		return self.user.email
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+def thumbnail(file):
+	print('file: ', file)
+	size = 75, 75
+	im = Image.open(file)
+	# filename, ext = file.filename.split('.')
+	# filename = filename + '-thumb.' + ext
+	im.thumbnail(size)
+	return im
+	# memory_file = cStringIO.StringIO()
+	# im.save(memory_file, ext)
+	# return memory_file, filename
+
+@receiver(post_save, sender=Employer)
+def my_callback(sender, instance, **kwargs):
+	print('sender: ', sender)
+	# employer = Employer.objects.get(pk=instance.pk)
+	# thumb = thumbnail(instance.business_license)
+	# # print('request finished', thumb)
+	# employer.business_license_thumb = thumb
+	# employer.save()
+	# return employer
+
+post_save.connect(my_callback, sender=Employer)		
