02f06483d6e9ff80503b3ec039fc7d2f290b1109
cassac <hicassac@gmail.com>
2016-05-12 21:54:38 -0700

    finalizes generage thumbnail helper function

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,17 +1,16 @@
 from PIL import Image
+import mimetypes
 from io import StringIO, BytesIO
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 def generate_thumbnail(file):
-	print('file:', file)
-
 	size = 75, 75
 	im = Image.open(file)
 	filename, ext = file.name.split('/')[-1].split('.')
+	mime = mimetypes.guess_type(file.name)
 	filename = filename + '-thumb.' + ext
 	im.thumbnail(size)
-
-	im.filename = filename
-	suf = SimpleUploadedFile(filename, im.tobytes(), content_type='image/jpeg')
-	print(suf.size)
-	return suf, filename
+	memory_file = BytesIO()
+	im.save(memory_file, ext)
+	suf = SimpleUploadedFile(filename, memory_file.getvalue(), content_type=mime[0])
+	return suf
