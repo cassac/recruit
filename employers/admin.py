bbe1165bbd5c8ab5ac9506d41b73e19a4c7e4cdb
cassac <hicassac@gmail.com>
2016-05-13 17:06:33 -0700

    creates admin panel for employers app

--- employers/admin.py
+++ employers/admin.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
 
-from .models import Employer, EmployerRequirements
+from .models import Employer, EmployerRequirements, EmployerImages
 
 class EmployerRequirementsInline(admin.StackedInline):
 	model = Employer
 	can_delete = False
 	verbose_name_plural = 'Preferences'
 
+class EmployerImagesInline(admin.StackedInline):
+	model = EmployerImages
+	can_delete = True
+	verbose_name_plural = 'Employer Images'
+	exclude = ('thumb',)
+
 class EmployerAdmin(admin.ModelAdmin):
 	# inlines = (EmployerRequirementsInline,)
+	inlines = (EmployerImagesInline,)
 	exclude = ('password', 'last_login', 'is_staff', 'business_license_thumb')
 
 admin.site.register(Employer, EmployerAdmin)
