145c4112ee0f1e25149c94b85c661d0af1064114
cassac <hicassac@gmail.com>
2016-05-14 00:12:24 -0700

    enlarges generated thumbnail size

--- recruit/utils.py
+++ recruit/utils.py
@@ -4,7 +4,7 @@ from io import StringIO, BytesIO
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 def generate_thumbnail(file):
-	size = 75, 75
+	size = 100, 100
 	im = Image.open(file)
 	filename, ext = file.name.split('/')[-1].split('.')
 	mime = mimetypes.guess_type(file.name)
