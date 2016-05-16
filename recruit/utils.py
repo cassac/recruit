a6b651a3f2a874776e6b7d52ef9a73042aae8686
cassac <hicassac@gmail.com>
2016-05-16 12:05:06 -0700

    fixes bug to accommodate images ending in .jpg

--- recruit/utils.py
+++ recruit/utils.py
@@ -6,13 +6,14 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 def generate_thumbnail(file):
 	size = 100, 100
 	im = Image.open(file)
-	filename, ext = file.name.split('/')[-1].split('.')
-	mime = mimetypes.guess_type(file.name)
-	filename = filename + '-thumb.' + ext
+	filename = file.name.split('/')[-1].split('.')[0]
+	mime = mimetypes.guess_type(file.name)[0]
+	file_type = mime.split('/')[-1]
+	filename = filename + '-thumb.' + file_type
 	im.thumbnail(size)
 	memory_file = BytesIO()
-	im.save(memory_file, ext)
-	suf = SimpleUploadedFile(filename, memory_file.getvalue(), content_type=mime[0])
+	im.save(memory_file, file_type)
+	suf = SimpleUploadedFile(filename, memory_file.getvalue(), content_type=mime)
 	return suf
 
 def delete_from_s3(instances_list):
