2a935fbb46064c8e2bb5f40cbe9dd48f03b6aef1
cassac <hicassac@gmail.com>
2016-05-16 14:16:24 -0700

    creates profile on user creation with signal

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from PIL import Image
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 from django.utils import timezone
 from phonenumber_field.modelfields import PhoneNumberField
 
@@ -13,16 +14,20 @@ from recruit.choices import (TIMEZONE_CHOICES, COUNTRY_CHOICES, GENDER_CHOICES,
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	timezone = models.CharField(max_length=50, blank=True)
+	timezone = models.CharField(choices=TIMEZONE_CHOICES, max_length=50, blank=True)
 	citizenship = models.CharField(max_length=50, blank=True)
 	user_type = models.CharField(
 		choices = (('Candidate', 'Candidate'), ('Recruiter', 'Recruiter'), ('Employer', 'Employer'),),
 		max_length = 50,
-		default = False,
 	)
-	is_admin = models.BooleanField(default=False)
 	last_modified = models.DateTimeField(auto_now_add=False, auto_now=True)
 	created = models.DateTimeField(auto_now_add=True, auto_now=False)
 
 	def __str__(self):
 		return self.user.email
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+post_save.connect(create_user_profile, sender=User)
