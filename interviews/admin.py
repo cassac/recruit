804d959b030eea8bb0e2a74f41b4419924e1fa03
cassac <hicassac@gmail.com>
2016-05-17 11:54:26 -0700

    extends search fileds for interviews

--- interviews/admin.py
+++ interviews/admin.py
@@ -3,14 +3,23 @@ from django.contrib import admin
 from .models import InterviewRequest, InterviewInvitation, Available
 
 class InterviewRequestAdmin(admin.ModelAdmin):
-	pass
+	list_display = ('candidate', 'candidate_accepted', 'job', 'employer_accepted')	
+	search_fields = ('candidate__user__username', 'candidate__user__email',
+		'candidate__user__first_name', 'candidate__user__last_name',
+		'job__employer__user__username', 'job__employer__user__email',
+		'job__employer__user__first_name', 'job__employer__user__last_name',
+		'job__employer__name_english', 'job__employer__name_local',)
 
 admin.site.register(InterviewRequest, InterviewRequestAdmin)
 
 class InterviewInvitationAdmin(admin.ModelAdmin):
-	readonly_fields = ('uuid',)
-	search_fields = ('uuid',)
-	list_display = ('status', 'confirmed_time', 'uuid')	
+
+	search_fields = ('uuid', 'candidate__user__username', 'candidate__user__email',
+		'candidate__user__first_name', 'candidate__user__last_name',
+		'job__employer__user__username', 'job__employer__user__email',
+		'job__employer__user__first_name', 'job__employer__user__last_name',
+		'job__employer__name_english', 'job__employer__name_local',)
+	list_display = ('candidate', 'job', 'status', 'confirmed_time', 'uuid')	
 	list_filter = ('status', 'confirmed_time',)
 
 admin.site.register(InterviewInvitation , InterviewInvitationAdmin)
