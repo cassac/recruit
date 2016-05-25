ead02283116bfc1c2a4fe606dfec738af64e87fd
cassac <hicassac@gmail.com>
2016-05-24 20:31:07 -0700

    adds views for job details

--- jobs/views.py
+++ jobs/views.py
@@ -5,3 +5,8 @@ def view_jobs(request):
 	jobs = Job.objects.all()
 	context = {'jobs': jobs}
 	return render(request, 'jobs/jobs.html', context)
+
+def view_job_details(request, job_id):
+	job = Job.objects.get(id=job_id)
+	context = {'job': job}
+	return render(request, 'jobs/details.html', context)
