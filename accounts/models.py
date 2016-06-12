3b0e0cc0c7a2a44d7eef4ea1697c17e26de7ab40
cassac <hicassac@gmail.com>
2016-06-11 22:53:59 -0700

    moves token generation/verification to UserProfile model

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.utils import timezone
+from django.core import signing
+from django.core.signing import SignatureExpired, BadSignature
+
 from phonenumber_field.modelfields import PhoneNumberField
 from django_countries.fields import CountryField
 from allauth.account.models import EmailAddress
@@ -28,6 +31,23 @@ class UserProfile(models.Model):
 
 	def __str__(self):
 		return self.user.email
+
+	def generate_token(self):
+		email = self.user.email
+		token = signing.dumps({'email': email})
+		return token
+
+	@staticmethod
+	def verify_token(token, max_age=604800):
+		# default max_age is 7 days
+		try:
+			value = signing.loads(token, max_age=max_age)
+		except SignatureExpired:
+			return None
+		except BadSignature:
+			return None
+		user = User.objects.get(email=value['email'])
+		return user
 
 def create_account_emailaddress(sender, instance, created, **kwargs):
 	# Used for django-allauth
