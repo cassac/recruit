673398cb30d7f0d660e4128bfa8e071741224456
cassac <hicassac@gmail.com>
2016-06-12 00:42:13 -0700

    finishes application form step2

--- candidates/views.py
+++ candidates/views.py
@@ -5,12 +5,13 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 from accounts.models import UserProfile
 
+from .models import Candidate, CandidateDocument
 from .forms import UserApplyStep1Form, UserApplyStep2Form
 
 def apply_step_1(request):
 
 	key = request.GET.get('key', None)
-	candidate = UserProfile.verify_token(key)
+	user = UserProfile.verify_token(key)
 
 	if not key and request.method == 'POST':
 		
@@ -55,10 +56,48 @@ def apply_step_1(request):
 	elif not key and request.method == 'GET':
 		form = UserApplyStep1Form()
 
-	elif key and request.method == 'GET':
+	elif key and user and request.method == 'POST':
+
+		form = UserApplyStep2Form(request.POST, request.FILES)
+	
+		if form.is_valid():
+			files = form.files
+			data = form.data
+	
+			try:
+				candidate = user.candidate
+				candidate.birth_year = data['birth_year']
+				candidate.gender = data['gender']
+				candidate.education = data['education']
+				candidate.education_major = data['education_major']
+				candidate.image = files['image']
+				candidate.save()
+			except Candidate.DoesNotExist:
+				candidate = Candidate.objects.create(user=user, 
+					birth_year=data['birth_year'],
+					gender=data['gender'],
+					education=data['education'],
+					education_major=data['education_major'],
+					image=files['image']
+					)
+			CandidateDocument.objects.create(
+				candidate=candidate,
+				document=files['resume'],
+				document_type='Resume'
+				)
+
+			messages.add_message(request, messages.SUCCESS,
+				'Form submitted successfully.')
+
+			# handle redirect
+
+	elif key and user and request.method == 'GET':
 		form = UserApplyStep2Form()
 
-	elif key and request.method == 'POST':
-		form = UserApplyStep2Form(request.POST)
+	else:
+		messages.add_message(request, messages.ERROR,
+			'A valid application key is required to submit documents. ' +
+			'Please contact the administrator.')
+		form = None
 
 	return render(request, 'candidates/apply.html', {'form': form})
